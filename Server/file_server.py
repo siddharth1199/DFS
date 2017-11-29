9c37b3d15f800ce7d936c2527b48be87727b01a1
siddharth1199 <siddharth1199@gmail.com>
2017-11-29 12:11:41 +0000

    Added threads

--- Server/file_server.py
+++ Server/file_server.py
@@ -3,45 +3,73 @@ import socket
 import select
 import sys
 from thread import *
-import threading
+from threading import Thread
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 5003          # Reserve a port for your service.
-s.bind(('', port))        # Bind to the port
+class client_thread(Thread):
 
-s.listen(5)                 # Now wait for client connection.
-path = os.getcwd()
-dir = os.listdir(path)
-print 'Server started'
-#while True:
-c, addr = s.accept()     # Establish connection with client.
-print 'Got connection from', addr
-a = c.recv(1024)
-remove = a[1:-1]
-h = remove.split(", ")
-h1 = h[0].replace("'", "")
-f1 = h[1]
+       def __init__(self,addr,c):
+              Thread.__init__(self)
+              self.addr = addr
+              self.c = c
 
-if f1 == "'r'":
-       print "Read"
-       f = open(h1)
-       l = f.read(1024)
-       while (l):
-              c.send(l)
-              print('Sent ',repr(l))
+       def run(self):
+              while True:
+                     file_server(self.addr,self.c)
+              
+
+def file_server(name,c):
+
+       path = os.getcwd()
+       dir = os.listdir(path)
+       print 'Server started'
+
+
+       a = c.recv(1024)
+       remove = a[1:-1]
+       h = remove.split(", ")
+       h1 = h[0].replace("'", "")
+       f1 = h[1]
+
+       if f1 == "'r'":
+              print "Read"
+              f = open(h1)
               l = f.read(1024)
-       f.close()
-       print "File was sent"
-elif f1 == "'a'":
-       a1 = c.recv(1024)
-       print a1
-       f = open(h1, 'a')
-       #l1 = f.read(1024)
-       l = f.write(a1)
-       print "File Modified"
-       f.close()
+              while (l):
+                     c.send(l)
+                     print('Sent ',repr(l))
+                     l = f.read(1024)
+              f.close()
+              print "File was sent"
+       elif f1 == "'a'":
+              a1 = c.recv(1024)
+              print a1
+              f = open(h1, 'a')
+              #l1 = f.read(1024)
+              l = f.write(a1)
+              print "File Modified"
+              f.close()
 
-else:
-       print "Failed to Read"
-                    # Close the connection
+       else:
+              print "Failed to Read"
+                           # Close the connection
+
+
+def Main():
+    host = '127.0.0.1'
+    port = 5003
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.bind(('',port))
+    s.listen(5)
+
+    
+    print('server started')
+    while(True):
+        (c,addr) = s.accept()
+        print('client connected ip:'+str(addr))
+        Thread = client_thread(addr,c)
+        Thread.start()
+
+
+        
+if __name__ == '__main__':    
+    Main()
