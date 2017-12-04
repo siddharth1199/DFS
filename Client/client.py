29420204e511b6db1690dc166249f55637070989
siddharth1199 <siddharth1199@gmail.com>
2017-12-04 10:36:38 +0000

    Authentication added

--- Client/client.py
+++ Client/client.py
@@ -1,6 +1,27 @@
-import socket, pickle         # Import socket module
+import socket, pickle       
 
-def directory():
+def authentication():
+    input1 = raw_input("Enter 1 to login, 2 to sign up: ")
+    socket_authen.send(input1.encode())
+    username = raw_input("Enter username: ")
+    socket_authen.send(username.encode())
+    password = raw_input("Enter password: ")
+    socket_authen.send(password.encode())
+    print(username, password, input1)
+    
+    
+    
+    val = (socket_authen.recv(1024)).decode()
+    if val == 'true':
+        directory()
+    elif val == 'false':
+        print 'Incorrect password'
+        authentication()
+    else:
+        print 'Username already exists'
+        authentication()
+
+def directory():    
     inp = raw_input('Type 1 to create a new file, and 2 to access existing file ')
     if inp == '1':
         f_name = raw_input('enter file name ')        
@@ -10,13 +31,7 @@ def directory():
         
         data = socket_dir.recv(2048)
         files = pickle.loads(data)
-        print(files)
-        
-        file_name = raw_input("Enter File Name, type exit to terminate: ")
-        socket_dir.send(file_name.encode())    
-        data = socket_dir.recv(2048)
-        data1 = data.decode()
-        print data1
+        directory()
     else:
         file_name = raw_input("Enter File Name, type exit to terminate: ")
         socket_dir.send(file_name.encode())    
@@ -101,7 +116,7 @@ def lock(file_name):
 if __name__ == '__main__':
    
     host = 'localhost'
-    port_dir = 5000
+    port_dir = 5001
     socket_dir = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket_dir.connect((host, port_dir))
 
@@ -112,7 +127,9 @@ if __name__ == '__main__':
     port_lock = 6002
     socket_lock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket_lock.connect((host, port_lock))
-    
-    
-    directory()
+
+    port_authen = 5009
+    socket_authen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_authen.connect((host, port_authen))
+    authentication()
     #fileserver()
