58a07c27f90a445145e1be21d53a516e09442ae4
siddharth1199 <siddharth1199@gmail.com>
2017-12-07 21:08:09 +0000

    Fixed wrong file update

--- Server/directory_server.py
+++ Server/directory_server.py
@@ -1,13 +1,7 @@
-import os.path
-import socket
-import select
-import sys
-from thread import *
-import threading
+import socket, pickle
 from threading import Thread
-import sqlite3
-
-
+import threading
+import os
 
 
 class client_thread(Thread):
@@ -19,53 +13,48 @@ class client_thread(Thread):
 
        def run(self):
               while True:
-                     lock(self.addr,self.c)
+                     get_list(self.addr,self.c)
               
 
-def lock(name,c):
-    conn = sqlite3.connect('test.db')
-    print "Opened database successfully";
-    d_f=[]
-    d_s=[]
-    file_name = c.recv(1024)
-    print(file_name)
-    print('1')
-    cursor = conn.execute("SELECT file_name, status from files_list")
-    i=0
-    for row in cursor:       
-        print row[0]
-        print row[1]
-        if file_name == row[0]:
-               i = i+1
-    if i==0:
-           d_f = file_name
-           d_s = 'unlocked'
-           cursor = conn.execute("INSERT INTO files_list VALUES (?, ?)", (d_f, d_s))
-           conn.commit()
-    else:
-           print 'file exists'
-           cursor = conn.execute("SELECT status from files_list WHERE file_name = (?)", (file_name,))
-           for row in cursor:                  
-                  d_s = row[0]
-    c.send(d_s.encode())
-
-    f_name = c.recv(1024)
-    cursor = conn.execute("UPDATE files_list SET status = 'locked' WHERE file_name = (?)",(f_name,))
-    conn.commit()
-    print('file locked')
-    inp = c.recv(1024)
-    if inp == 'done':
-           cursor = conn.execute("UPDATE files_list SET status = 'unlocked' WHERE file_name = (?)",(f_name,))
-           conn.commit()
+def get_list(name,sock):
+    
+    inp = (sock.recv(2048)).decode()
+    print inp
+    
+    if inp == '1':
+           f_name = (sock.recv(2048)).decode()
+           info = (sock.recv(2048)).decode()
+           f = open(f_name,"w+")
+           f.write(info)
+           print('done')
+           f.close()
+    current_working_directory = os.getcwd()
+    os.chdir(current_working_directory)
+    files =[]
+    d1=[]
+    files = os.listdir(current_working_directory)
+    data=pickle.dumps(files)
+    
+    if inp == '2':
+           sock.send(data)
+           f_name = sock.recv(2048)
            
+           if f_name in files:
+               i=files.index(f_name)
+               data = current_working_directory+f_name+' Size '+str(os.path.getsize(files[i]))+' Bytes  Last modified ' +str(os.path.getctime(files[i]))      
+           else:
+               data = 'File does not exist'
+           data = data.encode()
+           
+    else:
+           data = 'program terminated'
+    sock.send(data)
     
-    
-    
-        
+
 
 def Main():
     host = '127.0.0.1'
-    port = 6002
+    port = 5001
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.bind(('',port))
     s.listen(5)
@@ -78,7 +67,16 @@ def Main():
         Thread = client_thread(addr,c)
         Thread.start()
 
+        
+if __name__ == '__main__':
+    
+    Main()
+    
 
         
-if __name__ == '__main__':    
-    Main()
+
+    
+
+
+
+    
